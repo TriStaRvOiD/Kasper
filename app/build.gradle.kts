4261834b0f7da1e65f4ba77b2930b55f22ff4671
Aditya <58824912+TriStaRvOiD@users.noreply.github.com>
2023-11-11 14:35:08 +0530

    Updated dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,7 +46,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.3"
+        kotlinCompilerExtensionVersion = "1.5.4"
     }
     packaging {
         resources {
@@ -62,7 +62,7 @@ tasks.withType(type = org.jetbrains.kotlin.gradle.tasks.KotlinCompile::class).co
 dependencies {
 
     //Room
-    val roomVersion = "2.5.2"
+    val roomVersion = "2.6.0"
     implementation("androidx.room:room-runtime:$roomVersion")
     implementation("androidx.room:room-ktx:$roomVersion")
     ksp("androidx.room:room-compiler:$roomVersion")
@@ -91,11 +91,11 @@ dependencies {
     implementation("com.google.accompanist:accompanist-permissions:0.30.1")
 
     //Hilt
-    implementation("com.google.dagger:hilt-android:2.48")
-    ksp("com.google.dagger:hilt-compiler:2.48")
-    implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
-    implementation("androidx.hilt:hilt-common:1.0.0")
-    ksp("androidx.hilt:hilt-compiler:1.0.0")
+    implementation("com.google.dagger:hilt-android:2.48.1")
+    ksp("com.google.dagger:hilt-compiler:2.48.1")
+    implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
+    implementation("androidx.hilt:hilt-common:1.1.0")
+    ksp("androidx.hilt:hilt-compiler:1.1.0")
 
     //Miscellaneous
     implementation(platform("androidx.compose:compose-bom:2023.09.02"))
@@ -105,8 +105,8 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.core:core-splashscreen:1.0.1")
-    implementation("androidx.activity:activity-compose:1.7.2")
-    implementation("androidx.navigation:navigation-compose:2.7.3")
+    implementation("androidx.activity:activity-compose:1.8.0")
+    implementation("androidx.navigation:navigation-compose:2.7.5")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.2")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
@@ -121,5 +121,5 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.3")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,22 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     //Android
-    id("com.android.application") version "8.1.2" apply false
+    id("com.android.application") version "8.1.3" apply false
 
     //Kotlin
-    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
 
     //AboutLibraries
     id("com.mikepenz.aboutlibraries.plugin") version "10.8.3" apply false
 
     //Hilt
-    id("com.google.dagger.hilt.android") version "2.48" apply false
+    id("com.google.dagger.hilt.android") version "2.48.1" apply false
 
     //Key hider
     id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin") version "2.0.1" apply false
 
     //Annotation Processing
-    id("com.google.devtools.ksp") version "1.9.10-1.0.13" apply false //ksp
+    id("com.google.devtools.ksp") version "1.9.20-1.0.14" apply false //ksp
 }
 
 tasks.register("clean", Delete::class) {
