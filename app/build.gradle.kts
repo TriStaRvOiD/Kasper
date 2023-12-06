740cfe3687e54b7233ac223b8bf8b795a322f0cf
Aditya <58824912+TriStaRvOiD@users.noreply.github.com>
2023-12-06 18:27:56 +0530

    Update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,9 +44,10 @@ android {
     }
     buildFeatures {
         compose = true
+        buildConfig = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.4"
+        kotlinCompilerExtensionVersion = "1.5.5"
     }
     packaging {
         resources {
@@ -62,7 +63,7 @@ tasks.withType(type = org.jetbrains.kotlin.gradle.tasks.KotlinCompile::class).co
 dependencies {
 
     //Room
-    val roomVersion = "2.6.0"
+    val roomVersion = "2.6.1"
     implementation("androidx.room:room-runtime:$roomVersion")
     implementation("androidx.room:room-ktx:$roomVersion")
     ksp("androidx.room:room-compiler:$roomVersion")
@@ -72,17 +73,17 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 
     //Calendar
-    implementation("com.kizitonwose.calendar:compose:2.4.0-beta01")
+    implementation("com.kizitonwose.calendar:compose:2.4.0")
 
     //Vico
-    implementation("com.patrykandpatrick.vico:compose-m3:1.12.0")
+    implementation("com.patrykandpatrick.vico:compose-m3:1.13.1")
 
     //Lottie
-    implementation("com.airbnb.android:lottie-compose:6.1.0")
+    implementation("com.airbnb.android:lottie-compose:6.2.0")
 
     //AboutLibraries
-    implementation("com.mikepenz:aboutlibraries-core:10.8.3")
-    implementation("com.mikepenz:aboutlibraries-compose:10.8.3")
+    implementation("com.mikepenz:aboutlibraries-core:10.9.2")
+    implementation("com.mikepenz:aboutlibraries-compose:10.9.2")
 
     //Data Store
     implementation("androidx.datastore:datastore-preferences:1.0.0")
@@ -91,21 +92,21 @@ dependencies {
     implementation("com.google.accompanist:accompanist-permissions:0.30.1")
 
     //Hilt
-    implementation("com.google.dagger:hilt-android:2.48.1")
-    ksp("com.google.dagger:hilt-compiler:2.48.1")
+    implementation("com.google.dagger:hilt-android:2.49")
+    ksp("com.google.dagger:hilt-compiler:2.49")
     implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
     implementation("androidx.hilt:hilt-common:1.1.0")
     ksp("androidx.hilt:hilt-compiler:1.1.0")
 
     //Miscellaneous
-    implementation(platform("androidx.compose:compose-bom:2023.09.02"))
+    implementation(platform("androidx.compose:compose-bom:2023.10.01"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.core:core-splashscreen:1.0.1")
-    implementation("androidx.activity:activity-compose:1.8.0")
+    implementation("androidx.activity:activity-compose:1.8.1")
     implementation("androidx.navigation:navigation-compose:2.7.5")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.2")
@@ -116,7 +117,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.09.02"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.10.01"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,16 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     //Android
-    id("com.android.application") version "8.1.3" apply false
+    id("com.android.application") version "8.2.0" apply false
 
     //Kotlin
     id("org.jetbrains.kotlin.android") version "1.9.20" apply false
 
     //AboutLibraries
-    id("com.mikepenz.aboutlibraries.plugin") version "10.8.3" apply false
+    id("com.mikepenz.aboutlibraries.plugin") version "10.9.2" apply false
 
     //Hilt
-    id("com.google.dagger.hilt.android") version "2.48.1" apply false
+    id("com.google.dagger.hilt.android") version "2.49" apply false
 
     //Key hider
     id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin") version "2.0.1" apply false
