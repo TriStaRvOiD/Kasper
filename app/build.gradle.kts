c97aa05895e8d9de55a17109c18fd69f0561127e
Aditya <58824912+TriStaRvOiD@users.noreply.github.com>
2023-12-18 11:22:20 +0530

    Update dependenices

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,7 +47,7 @@ android {
         buildConfig = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.5"
+        kotlinCompilerExtensionVersion = "1.5.6"
     }
     packaging {
         resources {
@@ -107,7 +107,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.core:core-splashscreen:1.0.1")
     implementation("androidx.activity:activity-compose:1.8.1")
-    implementation("androidx.navigation:navigation-compose:2.7.5")
+    implementation("androidx.navigation:navigation-compose:2.7.6")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.2")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     id("com.android.application") version "8.2.0" apply false
 
     //Kotlin
-    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.21" apply false
 
     //AboutLibraries
     id("com.mikepenz.aboutlibraries.plugin") version "10.9.2" apply false
@@ -16,7 +16,7 @@ plugins {
     id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin") version "2.0.1" apply false
 
     //Annotation Processing
-    id("com.google.devtools.ksp") version "1.9.20-1.0.14" apply false //ksp
+    id("com.google.devtools.ksp") version "1.9.21-1.0.16" apply false //ksp
 }
 
 tasks.register("clean", Delete::class) {
