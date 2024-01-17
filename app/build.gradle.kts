1e97eae852215dd64dbb780d62ce2d62b2bc452b
Aditya <58824912+TriStaRvOiD@users.noreply.github.com>
2024-01-17 12:10:11 +0530

    Update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,7 +47,7 @@ android {
         buildConfig = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.6"
+        kotlinCompilerExtensionVersion = "1.5.8"
     }
     packaging {
         resources {
@@ -73,7 +73,7 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 
     //Calendar
-    implementation("com.kizitonwose.calendar:compose:2.4.0")
+    implementation("com.kizitonwose.calendar:compose:2.4.1")
 
     //Vico
     implementation("com.patrykandpatrick.vico:compose-m3:1.13.1")
@@ -82,8 +82,8 @@ dependencies {
     implementation("com.airbnb.android:lottie-compose:6.2.0")
 
     //AboutLibraries
-    implementation("com.mikepenz:aboutlibraries-core:10.9.2")
-    implementation("com.mikepenz:aboutlibraries-compose:10.9.2")
+    implementation("com.mikepenz:aboutlibraries-core:10.10.0")
+    implementation("com.mikepenz:aboutlibraries-compose:10.10.0")
 
     //Data Store
     implementation("androidx.datastore:datastore-preferences:1.0.0")
@@ -92,8 +92,8 @@ dependencies {
     implementation("com.google.accompanist:accompanist-permissions:0.30.1")
 
     //Hilt
-    implementation("com.google.dagger:hilt-android:2.49")
-    ksp("com.google.dagger:hilt-compiler:2.49")
+    implementation("com.google.dagger:hilt-android:2.50")
+    ksp("com.google.dagger:hilt-compiler:2.50")
     implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
     implementation("androidx.hilt:hilt-common:1.1.0")
     ksp("androidx.hilt:hilt-compiler:1.1.0")
@@ -106,13 +106,13 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.core:core-splashscreen:1.0.1")
-    implementation("androidx.activity:activity-compose:1.8.1")
+    implementation("androidx.activity:activity-compose:1.8.2")
     implementation("androidx.navigation:navigation-compose:2.7.6")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.2")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
-    implementation("androidx.lifecycle:lifecycle-viewmodel:2.6.2")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,22 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     //Android
-    id("com.android.application") version "8.2.0" apply false
+    id("com.android.application") version "8.2.1" apply false
 
     //Kotlin
-    id("org.jetbrains.kotlin.android") version "1.9.21" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 
     //AboutLibraries
-    id("com.mikepenz.aboutlibraries.plugin") version "10.9.2" apply false
+    id("com.mikepenz.aboutlibraries.plugin") version "10.10.0" apply false
 
     //Hilt
-    id("com.google.dagger.hilt.android") version "2.49" apply false
+    id("com.google.dagger.hilt.android") version "2.50" apply false
 
     //Key hider
     id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin") version "2.0.1" apply false
 
     //Annotation Processing
-    id("com.google.devtools.ksp") version "1.9.21-1.0.16" apply false //ksp
+    id("com.google.devtools.ksp") version "1.9.22-1.0.16" apply false //ksp
 }
 
 tasks.register("clean", Delete::class) {
